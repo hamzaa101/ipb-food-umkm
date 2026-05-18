43f826890655e69562f110d127c8b09afb5386cf
hamzaa101 <hamzahhudzaifah16@gmail.com>
2026-05-18 11:46:09 +0700

    feat(models): create product entity and seller relationship

---
+++ backend/app/models/product.py
@@ -0,0 +1,13 @@
+from sqlalchemy import String, Double, Float, ForeignKey, Boolean, Integer
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+import uuid
+from .base import Base
+
+class Product(Base):
+    __tablename__ = "products"
+    id: Mapped[str] = mapped_column(String, primary_key=True, default=lambda: str(uuid.uuid4()))
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+    price: Mapped[float] = mapped_column(Double, nullable=False)
+    seller_id: Mapped[str] = mapped_column(String, ForeignKey("sellers.id"), nullable=False)
+    
+    seller: Mapped["Seller"] = relationship("Seller", back_populates="products")
