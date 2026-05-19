14483b9624080aeccb3a778728d43d1ccd91135f
hamzaa101 <hamzahhudzaifah16@gmail.com>
2026-05-20 01:02:45 +0700

    feat(app): integrate CartProvider and fix image rendering issues

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,23 +5,26 @@ import Register from './pages/Register';
 import SelectRole from './pages/SelectRole';
 import RegisterSuccess from './pages/RegisterSuccess';
 import Dashboard from './pages/Dashboard';
+import { CartProvider } from './contexts/CartContext';
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        {/* Halaman pertama kali aplikasi dibuka */}
-        <Route path="/" element={<Welcome />} />
-        
-        {/* Rute Autentikasi */}
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/register/role" element={<SelectRole />} />
+      <CartProvider>
+        <Routes>
+          {/* Halaman pertama kali aplikasi dibuka */}
+          <Route path="/" element={<Welcome />} />
+
+          {/* Rute Autentikasi */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/register/role" element={<SelectRole />} />
           <Route path="/register/success" element={<RegisterSuccess />} />
-        
-        {/* Halaman Utama setelah Login */}
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
+
+          {/* Halaman Utama setelah Login */}
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </CartProvider>
     </BrowserRouter>
   );
 }
